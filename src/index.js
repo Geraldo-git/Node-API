8561a4b673d61d7514dd029aa1934049c1243793
Geraldo-dev <laboratorio.inicial@gmail.com>
2023-02-13 08:00:36 -0300

    Implementacao do GET e POST

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const { process_params } = require('express/lib/router');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 app.use(express.json());
 
 //GET http://localhost:3000
 //GET http://localhost:3000/projects?title=Node&owner=Aluizio&page=1
 
-app.get('/projects', function (request, response) {
+const id = uuidv4();
+const projects = [];
 
-    const { title, owner, page } = request.query;
-    console.log(`Return data: ${title} ${owner} ${page} `);
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ]);
+app.get('/projects', function (request, response) {
+    return response.json(projects);
 })
 
-app.post('/projects/:id/:nome', function (request, response) {
+app.post('/projects', function (request, response) {
 
     const { nome, owner } = request.body;
-    console.log(`Request body => ${nome} ${owner}`);
-
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const project = {
+        id: uuidv4(),
+        nome,
+        owner
+    }
+    projects.push(project);
+    return response.status(201).json(project);
 })
 
 app.put('/projects/:id', function (request, response) {
 
-    const { id} = request.params;
+    const { id } = request.params;
     const { nome, owner } = request.body;
     console.log(`Dados: ${id} ${nome} ${owner}`);
 
